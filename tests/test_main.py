45c824449029479912c1e5ee0712ac76f6c05789
nazar-kh <nazar@zaltechapps.com>
2024-07-21 13:56:37 +0500

    project structure updated

---
+++ tests/test_main.py
@@ -0,0 +1,24 @@
+import unittest
+from fastapi.testclient import TestClient
+from app.main import app
+
+
+class TestFastAPI(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.client = TestClient(app)
+
+    def test_root(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Hello World"})
+
+    def test_say_hello(self):
+        response = self.client.get("/hello/John")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {"message": "Hello John"})
+
+
+if __name__ == "__main__":
+    unittest.main()
